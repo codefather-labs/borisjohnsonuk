9f0eade68399bc64390bd9e63b978731e667d7e6
COdefather <codefather.ru@gmail.com>
2022-08-24 16:48:18 +0300

    updates

--- translator_example.py
+++ translator_example.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 import asyncio
+from types import NoneType
 from urllib.parse import quote
 from urllib.request import urlopen
 from typing import Optional
@@ -340,8 +341,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--source_pdf_path", required=True)
     parser.add_argument("--output_dir_path", required=True)
+    parser.add_argument("--from_page", type=int, default=0)
+    parser.add_argument("--to_page", type=int)
 
     args = parser.parse_args()
+    from_page = args.from_page
+    to_page = args.to_page
+    if isinstance(to_page, NoneType):
+        to_page = 0
 
     pdf_absolute_path = os.path.abspath(args.source_pdf_path)  # get filename from command line
     output_dir_path = os.path.abspath(args.output_dir_path)
@@ -390,6 +397,8 @@ if __name__ == '__main__':
     # initialize Boris and make him doing his job
     boris = MarkdownBoris(
         source_path=pdf_absolute_path,
-        output_dir_path=output_dir_path
+        output_dir_path=output_dir_path,
+        from_page=from_page,
+        to_page=to_page
     )
     boris.fetch_pages()
